b23db9dcd20098aa7b3679fbaf8fc85ae8a55b0b
Takao Sumitomo <cattaka@mail.cattaka.net>
2016-10-09 00:35:29 +0900

    Add MainActivityTest.java

---
+++ app/src/androidTest/java/io/github/droidkaigi/confsched/activity/MainActivityTest.java
@@ -0,0 +1,20 @@
+package io.github.droidkaigi.confsched.activity;
+
+import android.support.test.runner.AndroidJUnit4;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by cattaka on 16/10/09.
+ */
+@RunWith(AndroidJUnit4.class)
+public class MainActivityTest {
+
+    @Test
+    public void check_start_activity() {
+
+    }
+}
